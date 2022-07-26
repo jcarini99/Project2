114a7a6e49d406d0c4b0f281a5def8806b1e3de6
jcarini99 <108739815+jcarini99@users.noreply.github.com>
2022-07-26 14:56:36 -0400

    Spring Created
    Made new Spring project with new name

---
+++ car-reservation/src/test/java/com/cars/CarReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
